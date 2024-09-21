e0fe09e245b83953d35cc28efa9b30ecc68aee02
G0LD3N-CR4FT <eduardomaster.tellis@gmail.com>
2024-09-21 13:49:42 -0300

    feat: Mais monstros adicionados

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,7 @@ public class Main {
             // Escolher Novo Inimigo
             Inimigos monstro = new Inimigos();
             monstro.convocarMonstro(jogador);
-            System.out.println(monstro.getMonstro().getFotoMostro());
+            System.out.println(monstro.getMonstro().getFotoMonstro());
             System.out.println(monstro.getMonstro().getEncontro());
 
             // Batalhar contra o Inimigo
